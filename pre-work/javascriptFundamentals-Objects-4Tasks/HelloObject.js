9b561e674a9857a42712a9ea1331af524b3d7879
Winnie Kelley <w642kelley@gmail.com>
2025-01-14 01:16:03 -0600

    Adding new folder for 4 tasks with a file

---
+++ pre-work/javascriptFundamentals-Objects-4Tasks/HelloObject.js
@@ -0,0 +1,17 @@
+// Task 1 https://javascript.info/task/hello-object
+// Write the code, one line for each action:
+
+//Create an empty object user.
+let user = {};
+
+//Add the property name with the value John.
+user.name = "John";
+
+//Add the property surname with the value Smith.
+user.surname = "Smith";
+
+//Change the value of the name to Pete.
+user.name = "Pete";
+
+//Remove the property name from the object.
+delete user.name;
